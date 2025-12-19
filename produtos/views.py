a1a6b3de2f301b7f577a5e1327862a51d98ca911
mcrtav <mcrtav@gmail.com>
2025-12-18 23:53:29 -0300

    2º commit - OK

--- produtos/views.py
+++ produtos/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from django.db.models import Q
 from produtos.models import Produto
 from produtos.serializers import ProdutoSerializer
 
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,16 @@
 # setup/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from produtos.views import ProdutoViewSets
+from usuarios.views import UsuarioViewSets
+
+# Criar apenas UM router para toda a aplicação
+router = DefaultRouter()
+router.register(r'usuarios', UsuarioViewSets, basename='usuarios')
+router.register(r'produtos', ProdutoViewSets, basename='produtos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('usuarios.urls')),
-    path('', include('produtos.urls'))  # ADICIONAR ESTA LINHA
+    path('', include(router.urls)),
 ]
